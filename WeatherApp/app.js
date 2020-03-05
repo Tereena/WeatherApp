3ddb434b6e4906fe4c51a4867d53f0b5238e0488
Tereena <39005871+Tereena@users.noreply.github.com>
2020-03-05 12:23:17 +0530

    Update app.js

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -4,7 +4,7 @@ var name=document.querySelector('.name');
 var desc=document.querySelector('.desc');
 var temp=document.querySelector('.temp');
 
-button.addEventListener('click',function(){
+async function getdata(){
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' +inputValue.value + '&appid=08222362b887418db5399c814820b83b')
     .then(response=>response.json())
     .then(data=>{
@@ -18,4 +18,31 @@ button.addEventListener('click',function(){
 
     })
     .catch(err=>alert("wrong city name!"))
-})
+}
+button.addEventListener('click',function(){
+    
+    if(inputValue.value == "")
+    {
+        alert("enter city name");
+    }
+    else{
+        var status = check_alert(inputValue.value);
+    if(status)
+    {
+        getdata();
+    }
+    else{
+
+    }}
+})    
+
+
+function check_alert(inputValue){
+    var regex=/^[A-Za-z]+$/;
+    if(regex.test(inputValue)==false){
+        alert("only alphabets are allowed");
+        return false;
+    }
+    else
+        return true;
+}
